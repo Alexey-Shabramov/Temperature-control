1418ee6e4807a82fa5ef0ebbbc206f16d966db6b
Davakin111n <fidvey@googlemail.com>
2015-11-24 21:18:04 +0200

    Update - fx training + some choices

--- src/main/java/temperature/control/controller/TemperatureController.java
+++ src/main/java/temperature/control/controller/TemperatureController.java
@@ -1,4 +1,16 @@
 package temperature.control.controller;
 
+import javafx.event.ActionEvent;
+
 public class TemperatureController {
+
+    public void analyzeDataForReport(ActionEvent actionEvent) {
+    }
+
+    public void saveNewReport(ActionEvent actionEvent) {
+    }
+
+    public void beginWarming(ActionEvent actionEvent) {
+        System.out.println("Запуск нагрева!");
+    }
 }
